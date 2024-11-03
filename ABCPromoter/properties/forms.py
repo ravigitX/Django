f4eafdb1f2237d0d1f5dac7ff680befe42f4f05a
ravigitX <senthilravi15@gmail.com>
2024-11-04 03:01:24 +0530

    cleared

--- ABCPromoter/properties/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Customer
-
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = Customer
-        fields = ['name', 'mobile', 'email', 'property']
--- ABCPromoter/properties/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('receipt/<int:pk>/', views.receipt, name='receipt'),
-    path('view-booking/', views.view_booking, name='view_booking'),
-]
